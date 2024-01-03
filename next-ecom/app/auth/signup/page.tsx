a7592b2394c645acfaff72fe443bff28b0a7ee7f
Dario Turin <dturin1997@gmail.com>
2024-01-02 21:16:35 -0600

    Video-24-udemy: Form State & Validation

--- next-ecom/app/auth/signup/page.tsx
+++ next-ecom/app/auth/signup/page.tsx
@@ -4,16 +4,50 @@ import React from "react";
 import AuthFormContainer from "@components/AuthFormContainer";
 import { Button, Input } from "@material-tailwind/react";
 import { XMarkIcon } from "@heroicons/react/24/outline";
+import { useFormik } from "formik";
+import * as yup from "yup";
+
+const validationSchema = yup.object().shape({
+  name: yup.string().required("Name is required"),
+  email: yup.string().email("Invalid email!").required("Email is required"),
+  password: yup
+    .string()
+    .min(8, "Password must be at least 8 characters.")
+    .required("Password is required"),
+});
 
 export default function SignUp() {
+  const {
+    values,
+    handleChange,
+    handleBlur,
+    handleSubmit,
+    isSubmitting,
+    errors,
+    touched,
+  } = useFormik({
+    initialValues: { name: "", email: "", password: "" },
+    validationSchema,
+    onSubmit: (values) => {
+      console.log(values);
+    },
+  });
   const formErrors: string[] = [];
+  console.log(errors);
+  const { email, name, password } = values;
 
   return (
-    <AuthFormContainer title="Create New Account">
-      <Input name="name" label="Name" />
-      <Input name="email" label="Email" />
-      <Input name="password" label="Password" type="password" />
-      <Button type="submit" className="w-full">
+    <AuthFormContainer title="Create New Account" onSubmit={handleSubmit}>
+      <Input name="name" label="Name" onChange={handleChange} value={name} />
+      <Input name="email" label="Email" onChange={handleChange} value={email} />
+      <Input
+        name="password"
+        label="Password"
+        type="password"
+        onChange={handleChange}
+        value={password}
+      />
+      <Button type="submit" className="w-full bg-blue-500 text-white">
         Sign up
       </Button>
       <div className="">
