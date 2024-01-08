0e374144b7c53c11b84f1dc3d0c2acab96eae90c
Dario Turin <dturin1997@gmail.com>
2024-01-07 23:19:19 -0600

    Video-32-udemy: Sending Emails

--- next-ecom/app/api/users/route.ts
+++ next-ecom/app/api/users/route.ts
@@ -2,6 +2,7 @@ import { NewUserRequest } from "@/app/types";
 import startDb from "@lib/db";
 import UserModel from "@models/userModel";
 import { NextResponse } from "next/server";
+import nodemailer from "nodemailer";
 
 export const POST = async (req: Request) => {
   const body = (await req.json()) as NewUserRequest;
@@ -12,8 +13,23 @@ export const POST = async (req: Request) => {
     ...body,
   });
 
+  const transport = nodemailer.createTransport({
+    host: "sandbox.smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+      user: "413f7b922ac666",
+      pass: "83a84ef34adc84",
+    },
+  });
+
+  await transport.sendMail({
+    from: "verification@nextecom.com",
+    to: newUser.email,
+    html: `<h1>Please verify your email by clicking on <a href="http://localhost:3000">this link</a></h1>`,
+  });
+
   console.log(await newUser.comparePassword("12345678"));
-  console.log(await newUser.comparePassword("1234567")); 
+  console.log(await newUser.comparePassword("1234567"));
 
   return NextResponse.json(newUser);
 };
