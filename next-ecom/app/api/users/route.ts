b4f9ef59c5acbed7d2af9b03b60095565f9a0419
Dario Turin <dturin1997@gmail.com>
2024-01-07 23:45:54 -0600

    Video-34-udemy: Generating Verification Token

--- next-ecom/app/api/users/route.ts
+++ next-ecom/app/api/users/route.ts
@@ -1,8 +1,10 @@
+import EmailVerificationToken from "@models/emailVerificationToken";
 import { NewUserRequest } from "@/app/types";
 import startDb from "@lib/db";
 import UserModel from "@models/userModel";
 import { NextResponse } from "next/server";
 import nodemailer from "nodemailer";
+import crypto from "crypto";
 
 export const POST = async (req: Request) => {
   const body = (await req.json()) as NewUserRequest;
@@ -11,6 +13,13 @@ export const POST = async (req: Request) => {
 
   const newUser = await UserModel.create({
     ...body,
+  });
+
+  const token = crypto.randomBytes(36).toString("hex");
+
+  await EmailVerificationToken.create({
+    user: newUser._id,
+    token,
   });
 
   const transport = nodemailer.createTransport({
@@ -22,10 +31,12 @@ export const POST = async (req: Request) => {
     },
   });
 
+  const verificationUrl = `http://localhost:3000/verify?token=${token}&userId=${newUser._id}`;
+
   await transport.sendMail({
     from: "verification@nextecom.com",
     to: newUser.email,
-    html: `<h1>Please verify your email by clicking on <a href="http://localhost:3000">this link</a></h1>`,
+    html: `<h1>Please verify your email by clicking on <a href="${verificationUrl}">this link</a></h1>`,
   });
 
   console.log(await newUser.comparePassword("12345678"));
