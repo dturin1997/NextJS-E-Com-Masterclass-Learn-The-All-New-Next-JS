0f58eb5368c75d1b21ec810d038a1f634a1424ff
Dario Turin <dturin1997@gmail.com>
2024-01-27 01:01:00 -0600

    Video-41-udemy: Sign in UI

--- next-ecom/app/auth/signup/page.tsx
+++ next-ecom/app/auth/signup/page.tsx
@@ -8,6 +8,7 @@ import { useFormik } from "formik";
 import * as yup from "yup";
 import { filterFormikErrors } from "@/app/utils/formikHelpers";
 import { toast } from "react-toastify";
+import Link from "next/link";
 
 const validationSchema = yup.object().shape({
   name: yup.string().required("Name is required"),
@@ -49,6 +50,11 @@ export default function SignUp() {
 
   const { email, name, password } = values;
 
+  type valueKeys = keyof typeof values;
+  const error = (name: valueKeys) => {
+    return errors[name] && touched[name] ? true : false;
+  };
+
   return (
     <AuthFormContainer title="Create New Account" onSubmit={handleSubmit}>
       <Input
@@ -57,6 +63,7 @@ export default function SignUp() {
         onChange={handleChange}
         onBlur={handleBlur}
         value={name}
+        error={error("name")}
       />
       <Input
         name="email"
@@ -64,6 +71,7 @@ export default function SignUp() {
         onChange={handleChange}
         onBlur={handleBlur}
         value={email}
+        error={error("email")}
       />
       <Input
         name="password"
@@ -72,6 +80,7 @@ export default function SignUp() {
         onChange={handleChange}
         onBlur={handleBlur}
         value={password}
+        error={error("password")}
       />
       <Button
         disabled={isSubmitting}
@@ -80,6 +89,12 @@ export default function SignUp() {
       >
         Sign up
       </Button>
+
+      <div className="flex items-center justify-between">
+        <Link href="/auth/signin">Sign in</Link>
+        <Link href="/auth/forget-password">Forget password</Link>
+      </div>
+
       <div className="">
         {formErrors.map((err) => {
           return (
