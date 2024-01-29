9461bcd3d10de8f9242eb3330599bfb556a357ba
Dario Turin <dturin1997@gmail.com>
2024-01-28 20:45:43 -0600

    Video-45-udemy: Guest Route

---
+++ next-ecom/app/(guest_routes)/layout.tsx
@@ -0,0 +1,16 @@
+import { auth } from "@/auth";
+import { redirect } from "next/navigation";
+import React, { ReactNode } from "react";
+
+interface Props {
+  children: ReactNode;
+}
+
+export default async function GuestLayout({ children }: Props) {
+  const session = await auth();
+  //console.log("auth session ", session);
+
+  if (session) return redirect("/");
+
+  return <div>{children}</div>;
+}
