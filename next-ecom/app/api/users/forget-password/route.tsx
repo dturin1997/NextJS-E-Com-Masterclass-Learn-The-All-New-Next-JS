ac1d7a79eda538e2b897aed17cd108e09060ab12
Dario Turin <dturin1997@gmail.com>
2024-01-30 19:53:42 -0600

    Video-49-udemy: Sending Link Email

--- next-ecom/app/api/users/forget-password/route.tsx
+++ next-ecom/app/api/users/forget-password/route.tsx
@@ -3,26 +3,52 @@ import UserModel from "@/app/models/userModel";
 import { ForgetPasswordRequest } from "@/app/types";
 import { NextResponse } from "next/server";
 import crypto from "crypto";
+import nodemailer from "nodemailer";
 
-const POST = async (req: Request) => {
-  const { email } = (await req.json()) as ForgetPasswordRequest;
+export const POST = async (req: Request) => {
+  try {
+    const { email } = (await req.json()) as ForgetPasswordRequest;
 
-  if (!email)
-    return NextResponse.json({ error: "Invalid email" }, { status: 401 });
+    if (!email)
+      return NextResponse.json({ error: "Invalid email" }, { status: 401 });
 
-  const user = await UserModel.findOne({ email });
+    const user = await UserModel.findOne({ email });
 
-  if (!user)
-    return NextResponse.json({ error: "user not found!" }, { status: 404 });
+    if (!user)
+      return NextResponse.json({ error: "user not found!" }, { status: 404 });
 
-  // Generate the token and send the link to the given email
+    // Generate the token and send the link to the given email
 
-  await PasswordResetTokenModel.findOneAndDelete({ user: user._id });
-  const token = crypto.randomBytes(36).toString("hex");
-  await PasswordResetTokenModel.create({
-    user: user._id,
-    token,
-  });
+    await PasswordResetTokenModel.findOneAndDelete({ user: user._id });
+    const token = crypto.randomBytes(36).toString("hex");
+    await PasswordResetTokenModel.create({
+      user: user._id,
+      token,
+    });
 
-  // Send the link to the given email
+    // Send the link to the given email
+    const resetPassLink = `${process.env.PASSWORD_RESET_URL}?token=${token}&userId=${user._id}`;
+
+    const transport = nodemailer.createTransport({
+      host: "sandbox.smtp.mailtrap.io",
+      port: 2525,
+      auth: {
+        user: "413f7b922ac666",
+        pass: "83a84ef34adc84",
+      },
+    });
+
+    await transport.sendMail({
+      from: "verification@nextecom.com",
+      to: user.email,
+      html: `<h1>Click on <a href="${resetPassLink}">this link</a> to reset your password.</h1>`,
+    });
+
+    return NextResponse.json({ message: "Please check your email." });
+  } catch (error) {
+    return NextResponse.json(
+      { error: (error as any).message },
+      { status: 500 }
+    );
+  }
 };
